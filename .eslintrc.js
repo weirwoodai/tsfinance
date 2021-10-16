459dcda183adb591f9db46e0672f24d0a33eb907
Ricard Mallafre <rikitzzz@gmail.com>
2021-10-16 15:58:29 +0200

    feat: proper handling of API errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,10 @@ module.exports = {
   },
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'prettier'
   ],
+  plugins: ['prettier'],
   rules: {
     semi: ['error', 'always'],
     quotes: ['error', 'single']
--- src/FinTen.ts
+++ src/FinTen.ts
@@ -36,37 +36,52 @@ export class FinTen {
   }
 
   async login(): Promise<void> {
-    const response = (
-      await this.http.post('/users/login', {
+    try {
+      const {
+        data: { token }
+      } = await this.http.post<LoginResponse>('/users/login', {
         username: this.username,
         password: this.password
-      })
-    ).data as LoginResponse;
+      });
 
-    this._token = response.token;
+      this._token = token;
+    } catch (ex) {
+      if (!ex.response) throw ex;
+
+      const { status, data } = ex;
+      if (status !== 400) throw ex;
+      if (data.error !== 'Invalid credentials') throw ex;
+      throw new Error(data.error);
+    }
   }
 
   async getTickers(): Promise<GetTickersResponse> {
-    return (await this.http.get('/company/tickers')).data as GetTickersResponse;
+    const { data } = await this.http.get<GetTickersResponse>('/company/tickers');
+    return data;
   }
 
   async getFilings(ticker: string): Promise<GetFilingsResponse> {
-    if (this._token === null) {
-      await this.login();
-    }
+    if (this._token === null) await this.login();
 
-    const params = {
-      ticker
-    };
+    const params = { ticker };
 
-    return (
-      await this.http.get('/company/filings', {
+    try {
+      const { data } = await this.http.get<GetFilingsResponse>('/company/filings', {
         params,
         headers: {
           Authorization: `Bearer ${this.token}`
         }
-      })
-    ).data as GetFilingsResponse;
+      });
+      return data;
+    } catch (ex) {
+      if (!ex.response) throw ex;
+
+      const { status } = ex.response;
+      if (status !== 401) throw ex;
+
+      this._token = null;
+      return await this.getFilings(ticker);
+    }
   }
 }
 
